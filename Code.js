85e75352faee20fb74b6c308f4e8c0311d2747ea
RaviPatwardhan <76563203+RaviPatwardhan@users.noreply.github.com>
2021-05-29 12:55:12 -0400

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+
+    fill("white")
+    textSize(15)
+    text("ATADASBA",700,120)
+    fill("lightblue")
+   text("Hint:Default decision!!", 700,140)
+    
+   
+    fill("white")
+    textSize(15)
+    text("CUTNIFON",500,150)
+    fill("lightblue")
+    text("Hint: The same as the rest!!", 500,170)
+// add code for displaying rest of the names and the hints.
+    
+
+}
